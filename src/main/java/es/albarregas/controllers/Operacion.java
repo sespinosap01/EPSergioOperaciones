00411107079b6fb0b1faed73d9f0ddf9c253c205
sespinosap01 <sergieteespinosa2001@gmail.com>
2023-04-11 21:28:27 +0200

    Inicio del proyecto

---
+++ src/main/java/es/albarregas/controllers/Operacion.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.albarregas.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Sergio
+ */
+@WebServlet(name = "Operacion", urlPatterns = {"/Operacion"})
+public class Operacion extends HttpServlet {
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html lang=\"es\">");
+            out.println("<head>");
+            out.println("<title>Servlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            if (request.getParameter("param") != null) {
+                out.println("<h1>El valor del par&aacute;metro es: " + request.getParameter("param") + "</h1>");
+            } else {
+                out.println("<h1>El contexto es: " + request.getContextPath() + "</h1>");
+            }
+            out.println("<p><a href=\".\">Volver</a></p>");            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html lang=\"es\">");
+            out.println("<head>");
+            out.println("<title>Servlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1> El valor del campo oculto es: " + request.getParameter("entrada") + "</h1>");
+            out.println("<p><a href=\".\">Volver</a></p>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
